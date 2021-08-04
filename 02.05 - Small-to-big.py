fb198d173cb1c5863024e0ac4f51ade49013daa2
JazInimicus <57916818+JazInimicus@users.noreply.github.com>
2021-08-04 13:26:24 +0200

    Finally solved 02.05

--- 02.05 - Small-to-big.py
+++ 02.05 - Small-to-big.py
@@ -1,5 +1,5 @@
-smol = string(input("Please put in the small letter : "))
+smol = ord(input("Please put in the small letter : "))
 
 smol -= 32
 
-print("The big letter to the small letter is : ")
+print("The big letter to the small letter is : " , chr(smol))
